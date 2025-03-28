c192353e7301235fbbc5068ca1897af810420e93
Bea-manu <136429203+Bea-manu@users.noreply.github.com>
2025-03-28 22:58:28 +0200

    update in html and js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ function displayPoem(response){
         strings: "roses are red",
         autoStart: true,
         delay: 1,
+        cursor: "",
+        loop: true
     });
 }
 function generatePoem(event){
     event.preventDefault();
 
     let topicInput = document.querySelector("#topic");
-    let context = document.querySelector("You are famous poet Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning");
-    let prompt = document.querySelector("").value;
+    let context = "You are famous poet Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let prompt = `topic: generate a simple love poem ${topicInput.value}`;
     let apiKey = "8f0ab40o57b12e2t3b934b3b4137cfbc";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
